2d2fa2e9b1e6b7ff37571ff9a499db72aa307847
Nodemana <85973825+Nodemana@users.noreply.github.com>
2024-05-29 23:09:55 +1000

    ESTOP Controller WIP

--- src/motor.c
+++ src/motor.c
@@ -83,8 +83,9 @@
  */
 
 #define FILTER_SIZE 10
-#define TIMER_TICKS_PER_SEC 10
+#define TIMER_TICKS_PER_SEC 100
 #define speedQUEUE_LENGTH (10)
+#define DECELERATION_RATE -1000
 
 struct Message
 {
@@ -144,6 +145,7 @@ int32_t acceleration_RPM_per_second_shared;
 int32_t desired_speed_RPM_shared;
 uint32_t next_duty_shared;
 int32_t integral_error = 0;
+TickType_t ESTOP_Tick_Count_Prev;
 
 /*
  * Time stamp global variable.
@@ -172,7 +174,7 @@ static void prvMotorControllerTask(void *pvParameters);
  * PID Controller
  */
 int32_t PID(int32_t desired_speed, int32_t current_speed, int32_t *integral_error);
-int32_t ESTOP_Controller(int32_t current_speed);
+int32_t ESTOP_Controller(int32_t current_speed, double elapsed_time);
 uint32_t RPM_to_Duty_Equation(int32_t RPM);
 
 int32_t GetAverage(int32_t *filter_pointer, uint32_t size);
@@ -218,11 +220,9 @@ void vCreateMotorTask(void)
 
 static void prvESTOPTask(void *pvParameters)
 {
-    for (;;)
-    {
-        if (xSemaphoreTake(xESTOPSemaphore, portMAX_DELAY) == pdPASS)
-        {
-
+    for(;;) {
+        if(xSemaphoreTake(xESTOPSemaphore, portMAX_DELAY) == pdPASS){
+            ESTOP_Tick_Count_Prev = xTaskGetTickCount();
             motor_control_state = E_STOPPING;
         }
     }
@@ -237,6 +237,12 @@ static void prvMotorTask(void *pvParameters)
     int32_t current_speed_RPM;
     int32_t desired_speed_RPM;
     int32_t integral_error = 0;
+    TickType_t ESTOP_Tick_Count_Now;
+    TickType_t ESTOP_Tick_Count;
+
+    double ESTOPTimeSinceLastTaskRun;
+
+
 
     bool stopping_flag = false;
 
@@ -354,9 +360,16 @@ static void prvMotorTask(void *pvParameters)
 
             break;
         case E_STOPPING:
-            if (xSemaphoreTake(xControllerSemaphore, portMAX_DELAY) == pdPASS)
-            {
-                next_duty_shared = RPM_to_Duty_Equation(ESTOP_Controller(current_speed_RPM));
+            if(xSemaphoreTake(xControllerSemaphore, portMAX_DELAY) == pdPASS){
+                
+                ESTOP_Tick_Count_Now = xTaskGetTickCount();
+                ESTOP_Tick_Count = ESTOP_Tick_Count_Now - ESTOP_Tick_Count_Prev;
+
+
+                ESTOPTimeSinceLastTaskRun = (double)ESTOP_Tick_Count_Now / configTICK_RATE_HZ;
+
+                
+                next_duty_shared = RPM_to_Duty_Equation(ESTOP_Controller(current_speed_RPM, ESTOPTimeSinceLastTaskRun));
                 if(next_duty_shared < 15){
                     next_duty_shared = 0;
                     setDuty(next_duty_shared);
@@ -365,7 +378,9 @@ static void prvMotorTask(void *pvParameters)
                 } else {
                     setDuty(next_duty_shared);
                 }
+                ESTOP_Tick_Count_Prev = ESTOP_Tick_Count_Now;
             }
+            desired_speed_RPM = 0;
             break;
         default:
             return -1;
@@ -419,7 +434,7 @@ static void prvSpeedSenseTask(void *pvParameters)
 
             revolutions_per_second_double = num_revs / TimeSinceLastTaskRun;
 
-            int32_t revolutions_per_second = (int)round(revolutions_per_second_double); // Timer runs at 1/10 of a second. 12 Hall states in one revolution.
+            int32_t revolutions_per_second = (int)round(revolutions_per_second_double); // Timer runs at 1/100 of a second. 12 Hall states in one revolution.
 
             int32_t revolutions_per_minute = revolutions_per_second * 60;
             // UARTprintf("RPM before filtered: %d\n", revolutions_per_minute);
@@ -607,9 +622,9 @@ int32_t PID(int32_t desired_speed, int32_t current_speed, int32_t *integral_erro
     return (int32_t)round(current_speed + total_error * Kp + (*integral_error_ptr * Ki)); // + (*integral_error_ptr * Ki)
 }
 
-int32_t ESTOP_Controller(int32_t current_speed) {
-    int32_t acceleration = -1000;
-    return (int32_t)round(current_speed + acceleration); 
+int32_t ESTOP_Controller(int32_t current_speed, double elapsed_time) {
+    int32_t change_in_speed = DECELERATION_RATE * elapsed_time;
+    return (int32_t)round(current_speed + change_in_speed); 
 }
 
 /*-----------------------------------------------------------*/
